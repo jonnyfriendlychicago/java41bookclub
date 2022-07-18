dbc738344ad7f039420c6ed2d55b6921b6f3d104
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-07-18 13:49:47 -0500

    first commit - template java app running

---
+++ src/main/java/com/jonfriend/java41bookclub/Java41bookclubApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java41bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java41bookclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java41bookclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonfriend/java41bookclub/Java41bookclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java41bookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java41bookclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
